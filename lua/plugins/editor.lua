e3fa098f896eae5d2bc862dba04a662e38a83ac7
Damian <damian2535.35@gmail.com>
2023-11-17 23:28:47 -0300

    config de autocmd, opciones y telescope

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,70 @@
+return {
+  "telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-file-browser.nvim",
+  },
+  keys = {
+    {
+      "nt",
+      function()
+        local telescope = require("telescope")
+
+        local function telescope_buffer_dir()
+          return vim.fn.expand("%:p:h")
+        end
+
+        telescope.extensions.file_browser.file_browser({
+          path = "%:p:h",
+          cwd = telescope_buffer_dir(),
+          respect_gitignore = false,
+          hidden = true,
+          grouped = true,
+          previewer = false,
+          initial_mode = "normal",
+          layout_config = { height = 40 },
+        })
+      end,
+    },
+  },
+  config = function(_, opts)
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local fb_actions = require("telescope").extensions.file_browser.actions
+
+    opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
+      wrap_results = true,
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "top" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+      mappings = {
+        n = {},
+      },
+    })
+
+    opts.pickers = {
+      diagnostics = {
+        theme = "ivy",
+        initial_mode = "normal",
+        layout_config = {
+          preview_cutoff = 9999,
+        },
+      },
+    }
+
+    opts.extensions = {
+      file_browser = {
+        theme = "dropdown",
+        hijack_netrw = true,
+        mappings = {
+          ["n"] = {
+            ["a"] = fb_actions.create,
+            ["h"] = fb_actions.goto_parent_dir,
+          },
+        },
+      },
+    }
+    telescope.setup(opts)
+    require("telescope").load_extension("file_browser")
+  end,
+}
