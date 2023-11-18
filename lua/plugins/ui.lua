f1dc4174130f29058663c6a9b62d4eda34d5e401
Damian <damian2535.35@gmail.com>
2023-11-18 06:57:29 -0300

    inicio y config de bufferline

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        mode = "buffers",
+        separator_style = "think",
+        always_show_bufferline = true,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        color_icons = true,
+      },
+    },
+  },
+}
